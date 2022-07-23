bbae57948d62329d48c17b817a911a12ed610bb5
Ntaufik9 <taufikhidayat.1402@gmail.com>
2022-07-23 13:32:57 +0700

    initial commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.app')
+@section('css')
+<style>
+    .menu-link {
+        color: blue;
+    }
+
+    .menu-link:hover {
+        text-decoration: none;
+    }
+</style>
+@endsection
+@section('content')
+<h2>PSIS 1932</h2>
+<hr>
+
+<p>Halo, Ahlan Wa Sahlan di PSIS Semarang !
+    Yoh Iso Yoh!!!
+</p><p>By Ntaufik_9</p>
+
+</div>
+@endsection
